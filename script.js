d9c6df4a9ed9e9baaee52f0ee90a32314cf60e95
Abraham JV <159210918+Branduwu@users.noreply.github.com>
2024-03-21 10:23:41 -0600

    actualizacion

--- script.js
+++ script.js
@@ -2,21 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
   const startBtn = document.getElementById("startBtn");
   const gameContainer = document.getElementById("gameContainer");
   const guessBtn = document.getElementById("guessBtn");
-  const instruction = document.getElementById("instruction");
   const linkBtn = document.getElementById("linkBtn");
   const noBtn = document.getElementById("noBtn");
-  const customConfirm = document.getElementById("custom-confirm");
-  const customMessage = document.getElementById("custom-message");
-  const customYes = document.getElementById("custom-yes");
-  const customNo = document.getElementById("custom-no");
 
-  let randomNumber;
-
+  let instruction; 
   startBtn.addEventListener("click", function() {
     startBtn.classList.add("d-none");
     gameContainer.classList.remove("d-none");
-    randomNumber = Math.floor(Math.random() * 100) + 1;
-    instruction.textContent = "Piensa en un número del 1 al 100.";
+    instruction = document.getElementById("instruction"); 
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    if (instruction) {
+      instruction.textContent = "Piensa en un número del 1 al 100.";
+    }
     noBtn.classList.remove("d-none");
     linkBtn.classList.remove("d-none");
   });
@@ -26,21 +23,15 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   guessBtn.addEventListener("click", function() {
-    const userGuess = randomNumber;
+    const userGuess = Math.floor(Math.random() * 100) + 1; 
     
-    // Mostrar el cuadro de diálogo personalizado
-    customMessage.textContent = `¿Es ${userGuess} tu número?`;
-    customConfirm.classList.add("show");
-
-    // Escuchar los clics en los botones del cuadro de diálogo personalizado
-    customYes.addEventListener("click", function() {
+  
+    const userInput = confirm(`¿Es ${userGuess} tu número?`);
+    if (userInput) {
       alert("¡Excelente número, bro!");
-      location.reload();
-    });
-
-    customNo.addEventListener("click", function() {
+    } else {
       alert("Chale");
-      location.reload();
-    });
+    }
+    location.reload();
   });
 });
